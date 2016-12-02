b6a227724efbcd4b6e0dda267941aed539802ff7
Alexandr Zhilkin <azhilkin2033@gmail.com>
2016-12-02 12:42:11 +0300

    Исправил, теперь всё работает (по крайней мере на моих тестах)

--- java tasks/edu_task_2.java
+++ java tasks/edu_task_2.java
@@ -10,20 +10,20 @@ public class edu_task_2 implements CommonPartFinder{
 
     @Override
     public String getMaxLengthCommonPart(String[] strings) {
-		int N = strings.length;
-		if (N == 0) {
-			return null;
-		}
+        if(strings.length == 0){
+            return null;
+        }
         int StartPos = 0;
         int EndPos = strings[0].length();
         int CurrLength = EndPos-StartPos;
+        int N = strings.length;
         while (CurrLength > 0){
             Pattern p = Pattern.compile(strings[0].substring(StartPos, EndPos));
             Matcher m;
             boolean InnerFlag = true;
             for (int i = 1; (i < N)&&InnerFlag; i++){
                 m = p.matcher(strings[i]);
-                InnerFlag = m.matches();
+                InnerFlag = m.find();
             }
             if (InnerFlag){
                 break;
@@ -47,11 +47,11 @@ public class edu_task_2 implements CommonPartFinder{
     @Override
     public String getMaxLengthCommonPrefix(String[] strings) {
         int N = strings.length;
-		if (N == 0) {
-			return null;
-		}
         String Common = "";
         boolean Flag = true;
+        if(N == 0){
+            return null;
+        }
         for (int i = 0; (i < strings[0].length())&&Flag; i++){
             for(int j = 1; (j < N)&&Flag; j++){
                 if(strings[0].charAt(i) != strings[j].charAt(i)){
